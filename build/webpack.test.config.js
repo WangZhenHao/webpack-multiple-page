eca63a5fe19c0f08bede45428fb863dcc45e37a7
WangZhenHao <1471047476@qq.com>
2019-09-02 20:00:34 +0800

    提交ts脚手架

---
+++ build/webpack.test.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const merge = require('webpack-merge');
+//将样式文件单独打包输出的文件由配置文件中的output属性指定
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// 用于优化或者压缩css资源
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");  
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const common = require('./webpack.base.config.js');
+const { assetsPath, getPageGenerate } = require('./utils.js');
+const pagesGenerate = getPageGenerate();
+const config = require('./config/index.js');
+
+const prodWebpackConfig = merge(common, {
+	mode: "none",
+	output: {
+		filename: assetsPath('js/[name].[chunkhash].js'),
+		path: config.build.path,
+		publicPath: config.build.publicPath,
+		// chunkFilename: 
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(c|sc)ss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					"sass-loader"
+				]
+			}
+		]
+	},
+	optimization: {
+	    splitChunks: {
+	      cacheGroups: {
+	        common: {
+	          chunks: 'initial',
+		      name: 'common',
+		      //入口文件引入同一个js三次以上
+		      minChunks: 3,
+		      minSize: 1,
+		      filename: assetsPath("js/[name].[chunkhash].js")
+	        }
+	      }
+	    }
+	},
+	plugins: [
+	    ...pagesGenerate.htmlWebpackPlugin,
+        new CleanWebpackPlugin(),
+	    new OptimizeCSSPlugin({
+	      cssProcessorOptions: { safe: true }
+	    }),
+	    new MiniCssExtractPlugin({
+    　　  filename: assetsPath("css/[name].[chunkhash].css") 
+        })
+        
+	]
+})
+module.exports = prodWebpackConfig;
